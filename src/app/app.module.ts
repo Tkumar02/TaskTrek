56b53aa50e1726eb1a95249b04a53094964779d2
Tkumar02 <ta_265@yahoo.co.uk>
2024-02-12 17:35:48 +0000

    add firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FitnessHomeComponent } from './components/fitness/fitness-home/fitness-home.component';
+import { WeightComponent } from './components/fitness/weight/weight.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import { WeightComponent } from './components/fitness/weight/weight.component';
-import { FitnessHomeComponent } from './components/fitness/fitness-home/fitness-home.component';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -15,12 +25,19 @@ import { FitnessHomeComponent } from './components/fitness/fitness-home/fitness-
     HomeComponent,
     NavComponent,
     WeightComponent,
-    FitnessHomeComponent,
+    FitnessHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"tasktrek-79ad7","appId":"1:525558836035:web:caceb2946773a2fc605df6","storageBucket":"tasktrek-79ad7.appspot.com","apiKey":"AIzaSyAfhJhhgViwJa1VlaJjUbuVtrErjV4cLXw","authDomain":"tasktrek-79ad7.firebaseapp.com","messagingSenderId":"525558836035"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production:false,
+    firebaseConfig: {
+    apiKey: "AIzaSyAfhJhhgViwJa1VlaJjUbuVtrErjV4cLXw",
+    authDomain: "tasktrek-79ad7.firebaseapp.com",
+    projectId: "tasktrek-79ad7",
+    storageBucket: "tasktrek-79ad7.appspot.com",
+    messagingSenderId: "525558836035",
+    appId: "1:525558836035:web:caceb2946773a2fc605df6"
+    }
+};
+
+
